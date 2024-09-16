5a4119426e0ea06094d0e064251e49c65fe94841
mariiapdavey <mariiapdavey@gmail.com>
2024-09-15 22:53:08 -0700

    created basic footer; added events.js; added images folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import Footer from "./Components/Footer"; 
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Eventra!</h1>
-    </div>
+    <>
+      <div>
+        <h1>Header Placeholder</h1>
+      </div>
+      <main>
+        <h1>Welcome to Eventra!</h1>
+      </main>
+      <Footer/>
+
+    </>
   );
 }
 
---
+++ frontend/src/Events.js
@@ -0,0 +1,92 @@
+const events = [
+    {
+      _id: 1,
+      eventName: 'Whitney Mongé w/ Alec Shaw',
+      date: 'Wednesday, September 18, 2024',
+      time: '7:30 PM',
+      venue: {
+        name: 'Mainstage Theatre',
+        address: '216 Union Street, Seattle'
+      },
+      description:
+        'Singer-songwriter & guitarist, Whitney Mongé learned to capture the power of authenticity while honing her skills as a street performer. With or without elevation of a stage, each part of her performance, from the passion of her powerful, smoky voice to the intensity of a whispered lyric, draws listeners into an embrace within each truthful moment of song. Whitney was raised with rhythm and blues in her blood, and while growing up in the Pacific Northwest, her music is heavily influenced by the alternative rock and indie folk scene of Seattle. Alec Shaw is a singer/songwriter and producer raised in the foothills of the cascades in Washington State, garnering attention as an artist whose sound has developed into a soulful combination of two large worlds of musical influence rooted in gospel and Appalachian folk. Vocally, he bridges a gap between Otis Redding and Jeff Buckley with a knack for engaging and ethereal arrangements.',
+      price: 20
+    },
+    {
+      _id: 2,
+      eventName: 'Foleada',
+      date: 'Tuesday, September 17, 2024',
+      time: '7:30 PM - 9:00 PM',
+      venue: {
+        name: 'Sea Monster Lounge',
+        address: '2202 N 45th St, Seattle, WA 98103, USA'
+      },
+      description: 'Foleada brings the hottest Forro in the Pacific NW to the Sea Monster Lounge.',
+      price: 15
+    },
+    {
+      _id: 3,
+      eventName: 'Soul Coughing Reunion Tour',
+      date: 'Wednesday, September 18, 2024',
+      time: '9:00 PM',
+      venue: {
+        name: 'The Showbox',
+        address: 'Seattle'
+      },
+      description:
+        'Soul Coughing, one of the most unique and influential bands of the last four decades, is returning to the stage fully-formed for the first time in 25 years. All four original members, Mike Doughty, Sebastian Steinberg, Mark degli Antoni and Yuval Gabay will be performing from coast to coast this September and October. Each exclusive performance will feature songs from their legendary catalog– El Oso, Irresistible Bliss and their genre-defining debut album Ruby Vroom which celebrates its 30th anniversary this fall.',
+      price: 45
+    },
+    {
+      _id: 4,
+      eventName: 'Palaye Royale with Diamante, Weathers, and I See Stars',
+      date: 'Thursday, September 19, 2024',
+      time: '8:00 PM',
+      venue: {
+        name: 'The Neptune Theatre',
+        address: 'Seattle'
+      },
+      description:
+        'Experience an electrifying night of rock as Palaye Royale takes the stage with special guests Diamante, Weathers, and I See Stars. Get ready for a high-energy performance filled with bold anthems and unforgettable live music.',  
+      price: 35,
+    },
+    {
+      _id: 5,
+      eventName: 'Colde "BLUEPRINT" North America Tour 2024',
+      date: 'Friday, September 20, 2024',
+      time: 'Doors at 7:00 PM, Show at 8:00 PM',
+      venue: {
+        name: 'The Moore Theatre',
+        address: 'Seattle'
+      },
+      description: 'Colde shares the stage with Milena and Basecamp at The Moore Theatre.',
+      price: 26
+    },
+    {
+      _id: 6,
+      eventName: 'Lisa Fox Group',
+      date: 'Saturday, September 21, 2024',
+      time: '5:00 PM',
+      venue: {
+        name: 'The Royal Room',
+        address: 'Seattle'
+      },
+      description:
+        'A spirited singer who covers jazz, classical, gospel, and folk genres with aplomb, Lisa Fox currently heads up the Lisa Fox Jazz Group, based in Seattle. You’ll find yourself humming along as Lisa infuses select jazz and familiar tunes with her sparkling energy, clear, melodic stylings, and imaginative phrasing.',
+        price: 25
+    },
+    {
+      _id: 7,
+      eventName: 'Tori Kelly with Maisy Kay',
+      date: 'Saturday, September 21, 2024',
+      time: '7:30 PM',
+      venue: {
+        name: 'Showbox SoDo',
+        address: 'Seattle'
+      },
+      description:
+        'Using her powerful voice and buoyant spirit, Tori Kelly has racked up Platinum certifications and hundreds of millions of streams to become one of pop music’s biggest stars. With TORI., her first full-length release on Epic Records, the two-time Grammy winner showcases her wide range of musical inspirations, namely, her love of Y2K-era pop and R&B.',
+      price: 35  
+    }
+  ];
+  
