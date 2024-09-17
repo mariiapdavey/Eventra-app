6021c35018039fe84a83fbd1e03596bb54c12590
mariiapdavey <mariiapdavey@gmail.com>
2024-09-17 10:44:44 -0700

    added link to the EventScreen

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const EventList = ({event}) => {
   return (
     <Card className='my-5 p-3 rounded'>
-        <Card.Img src={event.image} variant='top' />
+        <Link to={`/event/${event._id}`}>
+          <Card.Img src={event.image} variant='top' />
+        </Link>
         <Card.Body>
+          <Link to={`/event/${event._id}`}>
             <Card.Title as='div'>
                 <strong>{event.eventName}</strong>
             </Card.Title>
-
+          </Link>
             <Card.Text as='div'>
                 {event.venue.name}
                 < br />
