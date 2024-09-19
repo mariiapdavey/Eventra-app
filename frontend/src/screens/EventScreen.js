a60415c65fea46a3f9d787e5c6bf98b96f17d5d8
marisadiaz <Risa.diaz@gmail.com>
2024-09-19 10:23:58 -0400

    updated styling for EventScreen

--- frontend/src/screens/EventScreen.js
+++ frontend/src/screens/EventScreen.js
@@ -10,23 +10,24 @@ const EventScreen = () => {
 
   return (
     <>
-    <Link className ='btn btn-light my-3' to='/'>
+    <Link className ='btn btn-secondary text-dark my-3' to='/'>
       Go Back
     </Link>
     <Row>
       <Image src={event.image} 
       alt={event.eventName} 
-      fluid
+      fluid 
+      className="event-image"   //added class for CSS styling
       />
     </Row>
-    <Row>
+    <Row className="mt-2 align-items-stretch"> 
       
       <Col md={8}>
-        <ListGroup variant='flush'>        
+        <ListGroup variant='flush' className="h-100 d-flex flex-column">        
             <ListGroup.Item>
                 <h3>{event.eventName}</h3>
             </ListGroup.Item>
-            <ListGroup.Item>
+            <ListGroup.Item className="flex-grow-1">
               <h5><strong>About this event:</strong></h5>
               <br/>
               {event.description}  
@@ -34,35 +35,35 @@ const EventScreen = () => {
 
           </ListGroup>
       </Col>
-      <Col md={4}>
+      <Col md={4} className="h-100">
         
         <ListGroup variant='flush'>
             <ListGroup.Item> <strong>When:</strong> {event.date} </ListGroup.Item>
             <ListGroup.Item> <strong>Time:</strong> {event.time} </ListGroup.Item>
             <ListGroup.Item><strong>Venue:</strong> {event.venue.name} </ListGroup.Item>
             <ListGroup.Item><strong>Address:</strong> {event.venue.address} </ListGroup.Item>
-
-            <Card> 
+      
+            <Card className="card-price h-100 d-flex flex-column justify-content-between"> 
               <ListGroup variant='flush'>
                 <ListGroup.Item>
-                    <Row>
+                    <Row className="text-primary-emphasis">
                         <Col> Price:</Col>
                         <Col>
                             <strong>${event.price}</strong>
                         </Col>
                     </Row>
                 </ListGroup.Item>
-                <ListGroup.Item>
+                <ListGroup.Item className="mt-auto">
                   <Button
                       className ='btn-block'
                       type='button'
                   >
-                      Add To Card
+                      Add to Cart
                   </Button>
               </ListGroup.Item>
               </ListGroup>
             </Card>
-        </ListGroup>
+          </ListGroup> 
       </Col>
     </Row>
     </>
