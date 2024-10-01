446fd71f397725e80e51fabcee44f192bf886b3f
mariiapdavey <mariiapdavey@gmail.com>
2024-09-30 22:05:02 -0700

    Updated Seeder.js and Package.json

--- backend/seeder.js
+++ backend/seeder.js
@@ -10,10 +10,41 @@ connectDB()
 
 const importData = async () => {
     try {
+        await Event.deleteMany()
+        await User.deleteMany()
 
+        const createdUsers = await User.insertMany(users)
+        const adminUser = createdUsers[0]._id
+
+        const sampleEvents = events.map(event => {
+            return {...event, user: adminUser} 
+        })
+        await Event.insertMany(sampleEvents)
+
+        console.log('Data imported!')
+        process.exit(1)
 
 }   catch (error){
     console.error(`Error: ${error.message}`)
     process.exit(1)
     }
+}
+
+const destroyData = async () => {
+    try{
+        await Event.deleteMany()
+        await User.deleteMany()    
+
+        console.log('Data destroyed!')
+        process.exit(1)
+        
+    } catch (error){
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
+if (process.argv[2] === '-d'){
+    destroyData()
+} else {
+    importData()
 }
