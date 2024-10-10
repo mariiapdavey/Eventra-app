ce0bf5fc963322b72d97635524706c4be169e4cc
marisadiaz <Risa.diaz@gmail.com>
2024-10-10 09:56:03 -0400

    added import of CartScreen in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import EventScreen from "./screens/EventScreen";
+import CartScreen from './screens/CartScreen';
 
 const App = () => {
   return (
