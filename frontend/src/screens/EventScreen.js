61de894aa2785747ef4780f665651a864bd90b85
rits-9 <ritu.sharma27@outlook.com>
2024-10-10 00:19:33 -0700

    cart constant file

--- frontend/src/screens/EventScreen.js
+++ frontend/src/screens/EventScreen.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { useParams, Link, useNavigate } from 'react-router-dom';
-import {Row, Col, Image, Card, Button, ListGroup} from 'react-bootstrap';
+import {Row, Col, Image, Card, Button, ListGroup, Form} from 'react-bootstrap';
 import { listEventDetails } from '../actions/eventActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
