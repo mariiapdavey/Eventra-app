fce9ce2a1f745b0c6ba553f3ea5dbc46ef95a500
mariiapdavey <mariiapdavey@gmail.com>
2024-10-23 11:24:16 -0700

    eventController.js removed duplicate exports

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,12 +1,14 @@
 import asyncHandler from 'express-async-handler'
 import Event from '../models/eventModel.js'
 
-export const getEvents = asyncHandler(async (req, res) => {
+//removed duplicate export
+const getEvents = asyncHandler(async (req, res) => {
   const events = await Event.find({})
   res.json(events);
 })
 
-export const getEventById = asyncHandler(async (req, res) => {
+//removed duplicate export
+const getEventById = asyncHandler(async (req, res) => {
   const event = await Event.findById(req.params.id);
 
   if (event) {
